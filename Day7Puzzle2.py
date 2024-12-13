b01813a63eb41b4b5e8c2af3ad615dce7b31607b
Manuel Pertek <Manuel.Pertek@edeka.de>
2024-12-13 11:43:10 +0100

    Day7 Puzzle 1&2

---
+++ Day7Puzzle2.py
@@ -0,0 +1,41 @@
+from itertools import product
+
+#with open("Day7Puzzle1_Input_Sample.txt", "r") as file:
+with open("Day7Puzzle1_Input.txt", "r") as file:    
+    inputData = file.read()
+
+operatorsConst = ['+', '*', '||']
+splittedInputData = inputData.split('\n')
+sumOfTestValuesOfSolvedEquations = 0
+
+for equationIndex in range(len(splittedInputData)):
+    inputEquation = splittedInputData[equationIndex].split(':')
+
+    testValue = inputEquation[0]
+    numbers = inputEquation[1].split(' ')
+    numbers.remove('')
+    print("input equation from index ", equationIndex ," with test value: ", testValue, " and numbers: ", numbers)
+    # Generate all possible operator combinations
+    operatorCombinations = list(product(operatorsConst, repeat=len(numbers) - 1))
+        
+    for operatorCombination in operatorCombinations:
+        for numberIndex in range(len(numbers)):
+            if numberIndex == 0:
+                # First Number is start value
+                result = int(numbers[numberIndex])
+                continue
+
+            operator = operatorCombination[numberIndex - 1]    
+            if operator == '+':
+                result += int(numbers[numberIndex])
+            elif operator == '*':
+                result *= int(numbers[numberIndex])
+            elif operator == '||':
+                result = int(str(result) + numbers[numberIndex]) # Concatenate numbers as string
+
+        if result == int(testValue):
+            print("equation solved: test value: ", testValue, " numbers: ", numbers, " with operator combination: ", operatorCombination)
+            sumOfTestValuesOfSolvedEquations += int(testValue)
+            break
+
+print("Sum of test values of solved equations: ", sumOfTestValuesOfSolvedEquations)
